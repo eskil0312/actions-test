8b08927171e94e89a46ee4625ea464779c7a3bec
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2020-09-04 09:49:47 +0200

    Docs

---
+++ src/hooks/useAppAuth.ts
@@ -0,0 +1,15 @@
+
+/**
+ * Note: will always return string
+ * 
+ * @example
+ * const toke = useAppAuth("testApp");
+ * @returns
+ * authtoken string
+ * @param
+ * appId -string
+ */
+const useAppAuth = (appId: string) => {
+    return "auttoken"
+}
+export default useAppAuth
